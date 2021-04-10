1146a903065dd3ff8c3e6684c5349da2b9e71a9f
Zakariya Qureshi <zakariyaq313@gmail.com>
2021-04-10 23:59:07 +0530

    Updated

--- script.js
+++ script.js
@@ -6,3 +6,44 @@ navButtons.forEach(button => {
         button.classList.add("active-button");
     })
 });
+
+let navigation = document.querySelector("nav");
+let middleSection = document.querySelector(".middle-section");
+let rightPanel = document.querySelector(".right-panel");
+let themeSwitch = document.querySelector(".theme-switcher");
+let darkTheme = document.querySelector(".theme-dark");
+let lightTheme = document.querySelector(".theme-light");
+
+themeSwitch.addEventListener("click", () => {
+    navigation.classList.toggle("darker");
+    middleSection.classList.toggle("dark");
+    rightPanel.classList.toggle("darker");
+    darkTheme.classList.toggle("invisible");
+    lightTheme.classList.toggle("invisible");
+})
+
+let ctx = document.querySelector("canvas");
+const config = {
+    type: 'line',
+    data,
+    options: {}
+};
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
